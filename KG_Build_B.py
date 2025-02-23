06bf32b3175713bac4a07b4cdf0734b379e25031
Eric <eric.es.she@gmail.com>
2025-02-23 07:58:15 +0000

    backup

--- KG_Build_B.py
+++ KG_Build_B.py
@@ -7,7 +7,7 @@ import os
 load_dotenv()
 
 # 連接到 Neo4j 資料庫，請確保 .env 中定義了 NEO4J_URI, NEO4J_USERNAME, NEO4J_PASSWORD
-uri = os.getenv("NEO4J_URI")
+uri = os.getenv("NEO4J_URI_B")
 username = os.getenv("NEO4J_USERNAME")
 password = os.getenv("NEO4J_PASSWORD")
 driver = GraphDatabase.driver(uri, auth=(username, password))
@@ -44,16 +44,21 @@ def create_case_data(tx, case_type, sim_input, sim_output):
         sim_input=sim_input, sim_output=sim_output
     )
 
+def delete_all_nodes(tx):
+    tx.run("MATCH (n) DETACH DELETE n")
+    print("Delete All Node")
+
 if __name__ == "__main__":
     # 載入 Excel 檔案，假設檔案中有欄位名稱：案件類型、模擬輸入、模擬輸出
-    df = pd.read_excel("DATA.xlsx")  # 請將 your_file.xlsx 換成你的檔案名稱
-
+    df = pd.read_excel("data.xlsx")  # 請將 your_file.xlsx 換成你的檔案名稱
+    with driver.session() as session:
+        session.execute_write(delete_all_nodes)
     # 使用 Neo4j session 逐筆將資料寫入資料庫
     with driver.session() as session:
         for index, row in df.iterrows():
-            case_type = row["案件類型"]
-            sim_input = row["模擬輸入"]
-            sim_output = row["模擬輸出"]
-            session.write_transaction(create_case_data, case_type, sim_input, sim_output)
+            case_type = row[0]
+            sim_input = row[1]
+            sim_output = row[2]
+            session.execute_write(create_case_data, case_type, sim_input, sim_output)
             
     driver.close()
